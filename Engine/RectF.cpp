b2498beeb8d7f576cbef3e0ed8e55b67bcf1e24b
Roahith Raj <roahithraj@gmail.com>
2017-06-25 20:07:42 +0530

    Added comments to RectF constructors

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -8,14 +8,17 @@ RectF::RectF(float leftin, float topin, float rightin, float bottomin)
 	right(rightin),
 	bottom(bottomin)
 {
+	//constructor 1
 }
 
 RectF::RectF(Vec2& topleft, Vec2& bottomright) {
+	//constructor 2
 	RectF(topleft.x, topleft.y, bottomright.x, bottomright.y);
 }
 
 
 RectF::RectF(Vec2& topleft, float width, float height) {
+	//constructor 3
 	RectF(topleft, Vec2(topleft.x + width, topleft.y + height));
 }
 
